3b135910557ab4ebb53cb9eb2d1fea08fdb5bfb0
patelaftab <10patelaftab@gmail.com>
2023-02-16 22:23:51 +0530

    [Aftab] :Deleting a Person Which Is Present Already

--- AddressBookProgram/Addressbook.cs
+++ AddressBookProgram/Addressbook.cs
@@ -8,22 +8,105 @@ namespace AddressBookProgram
 {
     public class Addressbook
     {
-        Contact contact=new Contact();
+        Contact contact = new Contact();
+        List<Contact> contactList = new List<Contact>();
         public void CreatContact()
         {
-            contact.FirstName=Console.ReadLine();
-            contact.LastName=Console.ReadLine();
-            contact.Email=Console.ReadLine();
-            contact.City=Console.ReadLine();
-            contact.PhoneNumber=Console.ReadLine();
-            contact.Address=Console.ReadLine();
-            contact.State=Console.ReadLine();
-            contact.Zip=Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the First Name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter the City Name");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter the State Name");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter the Zipcode");
+            contact.Zip = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Email");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("Enter the Phone Number");
+            contact.PhoneNumber = Console.ReadLine();
+            contactList.Add(contact);
+            Console.WriteLine();
         }
         public void Display()
         {
-            Console.WriteLine("Contact Details"+"\nFirst Name"+contact.FirstName+"\nLast Name"+contact.LastName+"\nEmail"+contact.Email+
-                "\nCity"+contact.City+"\nPhone Number"+contact.PhoneNumber+"\nAddress"+contact.Address+"\nZip Code"+contact.Zip+"\nState"+contact.State);
+            foreach (var contact in contactList)
+            {
+                Console.WriteLine("================\n*****Contact Details Of User =>=>=>" + "\nFirst Name:-" + contact.FirstName + "\nLast Name:-" + contact.LastName + "\nAddress:-" + contact.Address +
+                    "\nCity:-" + contact.City + "\nState:-" + contact.State + "\nZip Code:-" + contact.Zip + "\nPhone Number:-" + contact.PhoneNumber + "\nEmail:-" + contact.Email + "\n================");
+            }
+        }
+        public void EditContact(string name)
+        {
+            foreach (Contact contact in contactList)
+            {
+                if (contact.FirstName.Equals(name))
+                {
+                    Console.WriteLine("\nSelect 1: To Edit First Name\n" +
+                        "Select 2: To Edit Last Name\n" +
+                        "Select 3: To Edit Address\n" +
+                        "Select 4: To Edit City Name\n" +
+                        "Select 5: To Edit State Name\n" +
+                        "Select 6: To Edit Zip Code\n" +
+                        "Select 7: To Edit Email Address\n" +
+                        "Select 8: To Edit Phone NUmber\n");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter New Name");
+                            contact.FirstName = Console.ReadLine(); break;
+                        case 2:
+                            Console.WriteLine("Enter New Last Name");
+                            contact.LastName = Console.ReadLine(); break;
+                        case 3:
+                            Console.WriteLine("Enter New Address");
+                            contact.Address = Console.ReadLine(); break;
+                        case 4:
+                            Console.WriteLine("Enter New City Name");
+                            contact.City = Console.ReadLine(); break;
+                        case 5:
+                            Console.WriteLine("Enter New State Name");
+                            contact.State = Console.ReadLine(); break;
+                        case 6:
+                            Console.WriteLine("Enter New Zip Code");
+                            contact.Zip = Convert.ToInt32(Console.ReadLine()); break;
+                        case 7:
+                            Console.WriteLine("Enter New Email Address");
+                            contact.Email = Console.ReadLine(); break;
+                        case 8:
+                            Console.WriteLine("Enter Updated Phone Number");
+                            contact.PhoneNumber = Console.ReadLine(); break;
+                        case 9:
+                            Console.WriteLine("Invalid Option Please Choose Within Range 1-8"); break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("***********\nInvalid Name Choose Correct option\n***********");
+                    Console.WriteLine("Since You Have Change The First Name You Won't Be Able To Edit Further Details");
+                }
+            }
+        }
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter the first name of person to be removed");
+            string verifyingName = Console.ReadLine();
+            foreach (var contact in contactList)
+            {
+                if (contact.FirstName.Equals(verifyingName))
+                {
+                    contactList.Remove(contact);
+                    Console.WriteLine("Contact Deleted Sucessfully");
+                    break;
+                }
+                else
+                { Console.WriteLine("Enter Valid Name"); Console.ReadLine(); }
+            }
+
         }
     }
 }
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -12,9 +12,39 @@ namespace AddressBookProgram
         {
             Console.WriteLine("Welcome TO Address Book Problem");
             Addressbook addressbook = new Addressbook();
-            addressbook.CreatContact();
-            addressbook.Display();
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select 1: For Creating Contact\n" +
+                    "Select 2:Edit Existing Contact\n"+
+                    "Select 3:To Delete Contact\n"+
+                    "Select 4:To Display\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressbook.CreatContact();
+                        addressbook.Display();
+                        break;
+                    case 2:
+                        string name= Console.ReadLine();
+                        addressbook.EditContact(name);
+                        addressbook.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("enter Name To Delete");
+                        //name=Console.ReadLine();
+                        addressbook.DeleteContact();
+                        //addressbook.Display();
+                        break;
+                    case 4:
+                        addressbook.Display();
+                        break;
+                    case 5:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
